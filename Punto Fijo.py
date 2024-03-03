f8c6e83b6902c99ce535e1e4beb37557ce29bde2
normagomezb <160788864+normagomezb@users.noreply.github.com>
2024-03-02 22:41:37 -0600

    update

--- Punto Fijo.py
+++ Punto Fijo.py
@@ -1,25 +1,17 @@
 import numpy as np
 
-def Funcion(x):
-    fun = np.cos(x) - x
-    return(fun)
+def PuntoF(funcion, p0, No_iteraciones =100, error_r = 0.001):
+    i = 1
+    Error = 101
+    while i <= No_iteraciones and Error >= error_r:
+        p = funcion (p0)
+        Error = abs(p - p0)/abs(p)*100
+        if Error < error_r:
+            print('La raiz solucion es: ', p)
+            break
+        p0 = p
+        i += 1
+        if i > No_iteraciones:
+            print('Procedimiento completado sin exito')
     
-
-p0  = 1
-tolerancia = 0.001
-No_iteraciones = 20
-
-i = 1
-
-while i <= No_iteraciones:
-    p = Funcion (p0)
-    
-    if abs(p - p0) < tolerancia:
-        print('La solucion para p es: {0}'.format (p))
-        break
-    p0 = p
-    i = i + 1
-    
-    if i > No_iteraciones:
-        print('Procedimiento completado sin exito')
-
+PuntoF(lambda x: np.cos(x), 10, 100,0.001)
